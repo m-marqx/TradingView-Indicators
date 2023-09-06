16f6ef26ad11382745763df5e72f2d4faaf09f5a
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2023-09-06 18:02:01 -0300

    feat(MovingAverage): add _rma_pandas method

--- indicators/moving_average.py
+++ indicators/moving_average.py
@@ -108,3 +108,40 @@ class MovingAverage:
         sema = emas_df["sema"]
         return sema.dropna(axis=0)
 
+    def _rma_pandas(
+        self,
+        source: pd.Series,
+        length: int,
+        **kwargs
+    ) -> pd.Series:
+        """
+        Calculate the Relative Moving Average (RMA) of the input time series
+        data.
+
+        Parameters:
+        -----------
+        source : pandas.Series
+            The time series data to calculate the RMA for.
+        length : int
+            The number of periods to include in the RMA calculation.
+        **kwargs : additional keyword arguments
+            Additional keyword arguments to pass to the pandas EWM (Exponential
+            Weighted Moving Average) function.
+
+        Returns:
+        --------
+        pandas.Series
+            The calculated RMA time series data.
+
+        Note:
+        -----
+        The first values are different from the TradingView RMA.
+        """
+        sma = source.rolling(window=length, min_periods=length).mean()[:length]
+        rest = source[length:]
+        return (
+            pd.concat([sma, rest])
+            .ewm(alpha=1 / length, **kwargs)
+            .mean()
+        ).rename("RMA")
+
