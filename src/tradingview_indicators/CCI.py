e8da44e695873f2176a7108abf1efecfce7c6aaa
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2024-04-23 16:54:14 -0500

    refactor(CCI): change CCI class to a function

--- src/tradingview_indicators/CCI.py
+++ src/tradingview_indicators/CCI.py
@@ -3,157 +3,77 @@ from typing import Literal
 import pandas as pd
 import numpy as np
 from .errors_exceptions import InvalidArgumentError
-from .moving_average import ema
+from .moving_average import ema, sema, rma
 
 
-class CCI:
+def CCI(
+    source: pd.Series,
+    length: int = 20,
+    constant: float = 0.015,
+    method: Literal['sma', 'ema', 'dema', 'tema', 'rma'] = 'sma',
+) -> pd.DataFrame:
     """
-    A class for calculating Commodity Channel Index (CCI)
-    of time series data.
+    Calculate Commodity Channel Index (CCI)  of the input time series
+    data.
 
-    Attributes:
+    Parameters:
     -----------
-    source_arr : numpy.ndarray
-        The input time series data as a NumPy array.
     source : pd.Series
-        The input time series data as a DataFrame.
-    length : int
-        The number of periods to include in the CCI calculation.
-
-    Methods:
+        The input time series data.
+    length : int, optional
+        The number of periods to include in the CCI calculation
+        (default: 20)
+    constant : float, optional
+        The constant factor for CCI calculation.
+        (default: 0.015)
+    method : str, optional
+        The method to use for the moving average calculation.
+        (default: "sma")
+    Returns:
     --------
-    calculate_precice_CCI(smooth_column: str = "sma", constant: float = 0.015) -> CCI:
-        Calculate CCI using a precise method.
+    pd.DataFrame
+        The calculated CCI data as a DataFrame.
 
-    calculate_CCI(constant: float = 0.015, method: str = "sma") -> pd.DataFrame:
-        Calculate CCI using the specified method.
+    Raises:
+    -------
+    InvalidArgumentError
+        If the method is not 'sma', 'ema', 'sema', or 'rma'.
     """
+    source_arr = np.array(source)
 
-    def __init__(self, source: pd.Series, length: int = 20):
-        """
-        Initialize the CCI object.
-
-        Parameters:
-        -----------
-        source : pd.Series
-            The input time series data.
-        length : int, optional
-            The number of periods to include in the CCI calculation, by default 20.
-        """
-        self.source_arr = np.array(source)
-        self.source = source
-        self.length = length
-
-    def calculate_precice_CCI(
-        self,
-        smooth_column: str = "sma",
-        constant: float = 0.015,
-    ) -> pd.Series:
-        """
-        Calculate CCI using a precise method.
-        this version have more similar results from excel
-        than the standard version and TA-lib.
-
-        Parameters:
-        -----------
-        smooth_column : str, optional
-            The column to use for smoothing, by default "sma".
-        constant : float, optional
-            The constant factor for CCI calculation, by default 0.015.
-
-        Returns:
-        --------
-        CCI
-            The CCI object.
-        """
-
-        df = pd.DataFrame()
-        df["TP"] = self.source_arr
-        df["sma"] = df["TP"].rolling(self.length).mean()
-
-        df["mad"] = (
-            df["TP"]
-            .rolling(self.length)
-            .apply(lambda x: (pd.Series(x) - pd.Series(x).mean()).abs().mean())
+    match method:
+        case "sma":
+            ma = np.convolve(
+                source_arr,
+                np.ones(length) / length,
+                mode="valid"
+            )
+        case "ema":
+            ma = ema(source, length).to_numpy()
+        case "dema":
+            ma = sema(source, length, 2).to_numpy()
+        case "tema":
+            ma = sema(source, length, 3).to_numpy()
+        case  "rma":
+            ma = rma(source, length).to_numpy()
+        case _:
+            raise InvalidArgumentError(
+            "Method must be 'sma', 'ema', 'sema', or 'rma'."
         )
 
-        df["CCI"] = (
-            (df["TP"] - df[smooth_column])
-            / (constant * df["mad"])
-        )
+    window = np.lib.stride_tricks.sliding_window_view(
+        source_arr,
+        length,
+    )
 
-        return df["CCI"].dropna(axis=0, inplace=True)
+    mean_window = np.mean(window, axis=1)
+    abs_diff = np.abs(window - mean_window[:, np.newaxis])
+    mad = np.mean(abs_diff, axis=1)
 
-    def __set_sma(self):
-        """
-        Set the Simple Moving Average (SMA) for the CCI calculation.
+    df = pd.DataFrame()
+    df["source"] = source[length - 1 :]
+    df["mad"] = mad
+    df["ma"] = ma
+    df["CCI"] = (df["source"] - df["ma"])  / (constant * df["mad"])
 
-        Returns:
-        --------
-        CCI
-            The CCI object.
-        """
-        self.window = np.ones(self.length) / self.length
-        self.ma = np.convolve(self.source_arr, self.window, mode="valid")
-
-        return self
-
-    def __set_ema(self):
-        """
-        Set the Exponential Moving Average (EMA) for the CCI calculation.
-
-        Returns:
-        --------
-        CCI
-            The CCI object.
-        """
-        self.ma = ema(self.source, self.length).to_numpy()
-
-        return self
-
-    def calculate_CCI(self, constant: float = 0.015, method:Literal["ema", "sma"] = "sma") -> pd.DataFrame:
-        """
-        Calculate CCI using the specified method.
-
-        Parameters:
-        -----------
-        constant : float, optional
-            The constant factor for CCI calculation.
-            (default: 0.015)
-        method : str, optional
-            The method to use for the moving average calculation.
-            (default: "sma")
-
-        Returns:
-        --------
-        pd.DataFrame
-            The calculated CCI data as a DataFrame.
-
-        Raises:
-        -------
-        InvalidArgumentError
-            If the method is not 'sma' or 'ema'.
-        """
-        if method == "sma":
-            self.__set_sma()
-        elif method == "ema":
-            self.__set_ema()
-        else:
-            raise InvalidArgumentError("Method must be 'sma' or 'ema'.")
-
-        self.window = np.lib.stride_tricks.sliding_window_view(self.source_arr, self.length)
-        self.mean_window = np.mean(self.window, axis=1)
-        self.abs_diff = np.abs(self.window - self.mean_window[:, np.newaxis])
-
-        self.mad = np.mean(self.abs_diff, axis=1)
-
-        df = pd.DataFrame()
-        df["source"] = self.source[self.length - 1 :]
-        df["mad"] = self.mad
-        df["ma"] = self.ma
-        df["CCI"] = (
-            (df["source"] - df["ma"])
-            / (constant * df["mad"])
-        )
-
-        return df
+    return df
---
+++ tests/test_cci.py
@@ -0,0 +1,134 @@
+import unittest
+
+import pandas as pd
+import numpy as np
+from src.tradingview_indicators.CCI import CCI
+
+
+class TestCCI(unittest.TestCase):
+    def setUp(self):
+        np.random.seed(42)
+        self.source = pd.Series(np.random.randint(1, 500, 10))
+        self.length = 5
+
+    def test_CCI_sma(self):
+        np_values = np.array(
+            [
+                [107, 118.56, 253.2, -82.20872695],
+                [72, 126, 247, -92.59259259],
+                [189, 89.92, 197.6, -6.37603796],
+                [21, 78.4, 132, -94.3877551 ],
+                [103, 41.52, 98.4, 7.38599872],
+                [122, 43.92, 101.4, 31.26897389],
+            ]
+        )
+        columns = ["source", "mad", "ma", "CCI"]
+        indexes = [4,5,6,7,8,9]
+        ref_df = pd.DataFrame(np_values, columns=columns, index=indexes).astype('float64')
+        test_df = CCI(self.source, self.length, method='sma').astype('float64')
+
+        pd.testing.assert_frame_equal(ref_df,test_df)
+
+    def test_CCI_ema(self):
+        np_values = np.array(
+            [
+                [107.0, 118.55999999999999, 253.2, -82.20872694556905],
+                [72.0, 126.0, 192.8, -63.915343915343925],
+                [189.0, 89.92, 191.53333333333336, -1.8782127323052786],
+                [21.0, 78.4, 134.68888888888893, -96.67422524565386],
+                [103.0, 41.519999999999996, 124.12592592592596, -33.92088298960494],
+                [122.0, 43.92, 123.4172839506173, -2.15131140045128]
+            ]
+        )
+        columns = ["source", "mad", "ma", "CCI"]
+        indexes = [4,5,6,7,8,9]
+        ref_df = pd.DataFrame(np_values, columns=columns, index=indexes).astype('float64')
+        test_df = CCI(self.source, self.length, method='ema').astype('float64')
+
+        pd.testing.assert_frame_equal(ref_df,test_df)
+
+    def test_CCI_dema(self):
+        np.random.seed(42)
+        source = pd.Series(np.random.randint(1, 500, 20))
+        length = 5
+
+        np_values = np.array(
+            [
+                [107.0, 118.55999999999999, np.nan, np.nan],
+                [72.0, 126.0, np.nan, np.nan],
+                [189.0, 89.92, np.nan, np.nan],
+                [21.0, 78.4, np.nan, np.nan],
+                [103.0, 41.519999999999996, 179.26962962962963, -122.46247532053572],
+                [122.0, 43.92, 160.65218106995889, -58.67058450206266],
+                [467.0, 118.08000000000001, 186.41640603566532, 158.41440490307966],
+                [215.0, 124.31999999999998, 201.0431275720165, 7.4843803238864846],
+                [331.0, 121.12, 221.98354519128185, 60.004653681593],
+                [459.0, 120.24000000000001, 257.6255591119241, 111.6513866090463],
+                [88.0, 128.4, 254.61916984199573, -86.5104723997901],
+                [373.0, 113.35999999999999, 266.43642240625024, 62.669711593595494],
+                [100.0, 140.95999999999998, 253.19559883411335, -72.45346142362531],
+                [360.0, 145.6, 259.1779440427066, 46.16394503539076],
+                [152.0, 121.52000000000001, 249.9281036862251, -53.72399807232012],
+                [131.0, 114.64000000000001, 232.602829784875, -59.085153398973596]
+            ]
+        )
+
+        columns = ["source", "mad", "ma", "CCI"]
+        indexes = list(range(4,20))
+
+        ref_df = pd.DataFrame(np_values, columns=columns, index=indexes).astype('float64')
+        test_df = CCI(source, length, method='dema').astype('float64')
+        pd.testing.assert_frame_equal(ref_df,test_df)
+
+    def test_CCI_tema(self):
+        np.random.seed(42)
+
+        source = pd.Series(np.random.randint(1, 500, 20))
+        length = 5
+
+        np_values = np.array(
+            [
+                [107.0, 118.55999999999999, np.nan, np.nan],
+                [72.0, 126.0, np.nan, np.nan],
+                [189.0, 89.92, np.nan, np.nan],
+                [21.0, 78.4, np.nan, np.nan],
+                [103.0, 41.519999999999996, np.nan, np.nan],
+                [122.0, 43.92, np.nan, np.nan],
+                [467.0, 118.08000000000001, np.nan, np.nan],
+                [215.0, 124.31999999999998, np.nan, np.nan],
+                [331.0, 121.12, 315.5154836153026, 8.522961462294898],
+                [459.0, 120.24000000000001, 426.309255160968, 18.12527436184963],
+                [88.0, 128.4, 208.47283541917164, -62.55079720621581],
+                [373.0, 113.35999999999999, 310.723103546772, 36.62485088992473],
+                [100.0, 140.95999999999998, 164.83331695271292, -30.662749220919846],
+                [360.0, 145.6, 285.13889152281786, 34.2770643210541],
+                [152.0, 121.52000000000001, 193.97353927069148, -23.026958125242196],
+                [131.0, 114.64000000000001, 139.89768745991125, -5.174277424930945],
+            ]
+        )
+
+        columns = ["source", "mad", "ma", "CCI"]
+        indexes = list(range(4,20))
+
+        ref_df = pd.DataFrame(np_values, columns=columns, index=indexes).astype('float64')
+        test_df = CCI(source, length, method='tema').astype('float64')
+        pd.testing.assert_frame_equal(ref_df,test_df)
+
+    def test_CCI_rma(self):
+        np_values = np.array(
+            [
+                [107.0, 118.55999999999999, 253.2, -82.20872694556905],
+                [72.0, 126.0, 216.96, -76.69841269841271],
+                [189.0, 89.92, 211.36800000000002, -16.583629893238452],
+                [21.0, 78.4, 173.29440000000002, -129.50204081632657],
+                [103.0, 41.519999999999996, 159.23552, -90.29466923570972],
+                [122.0, 43.92, 151.788416, -45.21617486338799]
+            ]
+        )
+
+        columns = ["source", "mad", "ma", "CCI"]
+        indexes = [4,5,6,7,8,9]
+        ref_df = pd.DataFrame(np_values, columns=columns, index=indexes).astype('float64')
+        test_df = CCI(self.source, self.length, method='rma').astype('float64')
+
+        pd.testing.assert_frame_equal(ref_df,test_df)
